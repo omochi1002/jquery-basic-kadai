ff49ad924479ffc2f9af9f3c8e7d93e986f87952
omochi1002 <157349306+omochi1002@users.noreply.github.com>
2024-04-05 09:13:55 +0900

    Add files via upload

---
+++ kadai_004/event.js
@@ -0,0 +1,22 @@
+$(function() {
+  $(window).on({
+    
+    'load' : () => {
+      console.log('loadイベントが発生しました');
+    },
+
+    'scroll' : () => {
+      console.log('scrollイベントが発生しました');
+    },
+  });
+});
+
+// $(function() {
+//   $(window).on('load', function() {
+//     console.log('loadイベントが発生しました');
+//   });
+
+//   $(window).on('scroll', function() {
+//     console.log('scrollイベントが発生しました');
+//   });
+// });
