809f5b1df2c0eb8c00672665d53e7e6db27bc626
omochi1002 <157349306+omochi1002@users.noreply.github.com>
2024-04-08 15:57:53 +0900

    Add files via upload

---
+++ kadai_006/weather.js
@@ -0,0 +1,9 @@
+// $(window).on('load', function() {
+//   $('#target').addClass('heading');
+// });
+
+$(function() {
+  $(window).on('load', function() {
+    $('#target').addClass('heading');
+  });
+});
